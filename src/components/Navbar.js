c63063d87b1ead795876dbf715b38a3f801faf0b
Soumya_Goyal <somyagoyal739@gmail.com>
2020-01-09 11:47:18 -0800

    Added dependencies-bootstrap and react-dates, added 4 new classes in App.css and added component DateRangePicker as per airbnb react calendar in RangeCalendar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div>
-            <button>Home</button>
-            <button>Photos</button>
-            <button>Timeline</button>
-            <button>Profile</button>
-            <button>Logout</button>
+            <NavLink to="/home"><button>Home</button></NavLink>
+            <NavLink to="/photos"><button>Photos</button></NavLink>
+            <NavLink to="/timeline"><button>Timeline</button></NavLink>
+            <NavLink to="/profile"><button>Profile</button></NavLink>
+            <NavLink to="/"><button>Logout</button></NavLink>
         </div>
     )
 }
