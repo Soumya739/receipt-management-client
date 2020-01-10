3697710b48846e5491408f65ef626254f8eeb4a6
Soumya_Goyal <somyagoyal739@gmail.com>
2020-01-09 17:39:58 -0800

    created 'constructor', 'handleFormInput' and 'handleFormInput' methods, and imported 'connect' in signup.js

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,47 +1,72 @@
 import React, { Component } from 'react'
 import { Button, Modal, Checkbox, Form } from 'semantic-ui-react'
+import { connect } from 'react-redux';
 
 
 export class Signup extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            email: "",
+            contact_num: "",
+            city: "",
+            country: "",
+            state: "",
+            password: "",
+            password_confirmation: ""
+        }
+    }
+
+    handleFormInput = (e) => {
+        console.log(e.target.id, e.target.value);
+        this.setState({ ...this.state, [e.target.id]: e.target.value })
+    }
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
+
     render() {
+        let { username, email, contact_num, city, country, state, password, password_confirmation } = this.state
         return (
             <div>
                 <Modal trigger={<Button>Signup</Button>}>
                     <Modal.Header>Signup</Modal.Header>
                     <Modal.Content image>
                         <Modal.Description>
-                            <Form>
+                            <Form onSubmit={(e) => this.handleFormSubmit(e)}>
                                 <Form.Field>
                                     <label>Username:</label>
-                                    <input placeholder='Username' />
+                                    <input placeholder='Username' id="username" value={username} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Email:</label>
-                                    <input placeholder='name@example.com' />
+                                    <input placeholder='name@example.com' id="email" value={email} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Phone Number:</label>
-                                    <input placeholder='(xxx)-xxx-xxxx' />
+                                    <input placeholder='(xxx)-xxx-xxxx' id="contact_num" value={contact_num} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Country:</label>
-                                    <input placeholder='Select' />
+                                    <input placeholder='Country' id="country" value={country} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>State:</label>
-                                    <input placeholder='Select' />
+                                    <input placeholder='State' id="state" value={state} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>City:</label>
-                                    <input placeholder='name@example.com' />
+                                    <input placeholder='City' id="city" value={city} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Password:</label>
-                                    <input placeholder='Password' type='password' />
+                                    <input placeholder='Password' type='password' id="password" value={password} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Password Confirmation:</label>
-                                    <input placeholder='Password' type='password' />
+                                    <input placeholder='Password' type='password' id="password_confirmation" value={password_confirmation} onChange={(e) => this.handleFormInput(e)} required />
                                 </Form.Field>
                                 <Form.Field>
                                     <Checkbox label='I agree to the Terms and Conditions' />
@@ -49,7 +74,7 @@ export class Signup extends Component {
                                 <Button.Group>
                                     <Button negative>Cancel</Button>
                                     <Button.Or />
-                                    <Button positive>Signup</Button>
+                                    <Button positive type="submit">Signup</Button>
                                 </Button.Group>
                             </Form>
                         </Modal.Description>
@@ -60,5 +85,8 @@ export class Signup extends Component {
     }
 }
 
-export default Signup
+
+
+
+export default connect()(Signup)
 
